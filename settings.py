056e95a15253538ad363d9621529da63ea36c6bb
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-09-02 16:37:55 +0200

    release v2.0.0

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 SETTINGS = {
     "title" : "Speaker - Text to Speech",
     "author": "S3r43o3",
-    "version": "1.3.8",
+    "version": "2.0.0",
     "savepath": ".\\output\\",
     "testText": "Willkommen!\nDies ist eine Testausgabe deiner aktuell ausgewählten Stimme.",
     "github": "https://github.com/sera619/Speaker-TTS-Offline",
