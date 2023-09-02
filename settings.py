6d98e9c14f24cb5067eda302e2a499f1d7950833
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-09-02 16:29:06 +0200

    add social media URLs

--- settings.py
+++ settings.py
@@ -3,5 +3,8 @@ SETTINGS = {
     "author": "S3r43o3",
     "version": "1.3.8",
     "savepath": ".\\output\\",
-    "testText": "Willkommen!\nDies ist eine Testausgabe deiner aktuell ausgewählten Stimme."
+    "testText": "Willkommen!\nDies ist eine Testausgabe deiner aktuell ausgewählten Stimme.",
+    "github": "https://github.com/sera619/Speaker-TTS-Offline",
+    "youtube": "https://youtube.com/@S3R43o3?si=bDEtV7n7O9Cgzs0c",
+    "thm": "https://tryhackme.com/p/S3R43o3"
 }
